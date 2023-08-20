704f57a1e18c4f909ffec7e7f22434d2e21cc125
Avery <avery.carty7@gmail.com>
2023-08-20 09:52:11 -0400

    update: Home Page Info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,12 @@ import Head from "next/head";
 import HeaderNav from "~/components/HeaderNav";
 import SacramentoText from "~/components/SacramentoText";
 import Footer from "~/components/Footer";
+import Image from "next/image"
+
+import hha1 from "../assets/hha1.jpg"
+import hha2 from "../assets/hha2.jpg"
+import hha3 from "../assets/hha3.jpg"
+import hha4 from "../assets/hha4.jpg"
 
 export default function Home() {
   return (
@@ -21,16 +27,47 @@ export default function Home() {
 
       <hr className="h-px my-8 bg-sky-400 border-0"/>
 
-      <main className="flex flex-col items-center justify-center">
-        <div className="font-mono">
-          We care by providing personal Care Services, Compainion Service and Homemaking Services for your elderly and disabled.
+      <main className="flex flex-col justify-center justify-items-center items-center gap-12">
+        <div className="flex justify-center justify-items-center items-center">
+
+          <div className="flex flex-col flex-wrap w-10/12 justify-around gap-8">
+
+                <div className="flex flex-col rounded rounded-full border border-4 border-sky-500 text-xl">
+                  <div className="flex flex-row gap-8 justify-center justify-items-center items-center text-center">
+                    <Image className="w-1/3" src={hha2} alt="hha2"/>
+                    <div className="text-center">
+                      Welcome to Vero Lady HHA Services, your trusted partner in personalized home health care services. At Vero Lady HHA Services, we understand that every individual's journey to wellness is unique. That's why we're dedicated to providing compassionate and tailored care that promotes independence, enhances quality of life, and brings peace of mind to both our clients and their families.
+                    </div>
+                  </div>
+
+                  <div className="flex flex-row gap-8 justify-center justify-items-center items-center text-center">
+                    <div className="text-center">
+                      Our team of highly-trained caregivers is committed to delivering top-notch medical and non-medical assistance within the comfort of your own home. Whether you or your loved one are recovering from an illness, managing a chronic condition, or simply in need of a helping hand, we're here to provide the support you deserve.
+                    </div>
+                    <Image className="w-1/3" src={hha1} alt="hha1"/>
+                  </div>
+
+                  <div className="flex flex-row gap-8 justify-center justify-items-center items-center text-center">
+                    <Image className="w-1/3" src={hha4} alt="hha4"/>
+                    <div className="text-center">
+                      With a deep respect for the dignity and preferences of each person we serve, Vero Lady HHA Services offers a wide range of services that encompass everything from skilled nursing care to companionship and light housekeeping. Our holistic approach not only addresses physical needs but also fosters emotional well-being, ensuring a comprehensive and holistic caregiving experience.
+                    </div>
+                  </div>
+
+                  <div className="flex flex-row gap-8 justify-center justify-items-center items-center text-center">
+                    <div className="text-center">
+                      As a locally-owned and operated small business, we take pride in our strong ties to the community we serve. Our mission is to create a warm and nurturing environment where our clients feel valued and understood. We believe that everyone deserves to age gracefully and comfortably in the place they call home, and we're honored to be a part of that journey.
+                    </div>
+                    <Image className="w-1/3" src={hha3} alt="hha3"/>
+                  </div>
+                </div>
+
+          </div>
         </div>
-        <ul>
-          <li>Grooming/Dressing</li>
-          <li>Cleaning Services</li>
-          <li>Companionship</li>
-          <li>And More</li>
-        </ul>
+
+        <div className="rounded rounded-full border border-4 border-sky-500 text-xl w-1/4 my-12 text-center">
+          Explore our services to discover how Vero Lady HHA Services can make a positive difference in your life or the life of your loved one. Contact us today to schedule a consultation and take the first step towards a brighter, healthier future. Your well-being is our priority, and we're here to welcome you home.
+        </div>
       </main>
 
       <Footer />
