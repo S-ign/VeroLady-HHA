3b9bb12e38a5f491880cd8219ee4a898053e1454
Avery <avery.carty7@gmail.com>
2023-08-22 13:59:59 -0400

    update: Updated landing page style

---
+++ src/components/HeaderNavLanding.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from "next/link";
+import SacramentoText from "~/components/SacramentoText";
+
+const HeaderNav = () => {
+  return (
+    <div className="absolute flex flex-wrap w-full p-4 justify-between justify-items-between">
+
+      <Link href="/">
+        <div className="text-3xl">
+          <SacramentoText text="Vero Lady " />
+          <div className="border rounded-full p-2 border-sky-500 bg-sky-300 opacity-75">HHA Services</div>
+        </div>
+      </Link>
+      
+      <div>
+        <ul className="flex justify-between gap-8">
+          <Link className="p-4 text-lg" href="about"> <li>About</li> </Link>
+          <Link className="p-4 text-lg" href="services"> <li>Services</li> </Link>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderNav
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
+import Link from "next/link";
 import Image from "next/image"
 import Head from "next/head";
-import HeaderNav from "~/components/HeaderNav";
+import HeaderNavLanding from "~/components/HeaderNavLanding";
 import Footer from "~/components/Footer";
 import SacramentoText from "~/components/SacramentoText";
 
@@ -19,46 +20,53 @@ export default function Home() {
       </Head>
 
       <div className="flex flex-wrap justify-items-center justify-center gap-8">
-        <HeaderNav />
-        <div className="flex text-6xl">
+        <HeaderNavLanding />
+      </div>
+      <div className="flex items-center justify-center text-6xl bg-[url('../assets/hha-intro.jpg')] h-screen bg-cover bg-center bg-no-repeat">
+
+        <div className="flex flex-col gap-4">
           <SacramentoText text="Family Love, We Care"/>
+          <div className="items-center justify-center flex">
+            <Link className="w-full flex flex-wrap text-2xl p-2" href="get-started"> <button className="w-full border border-2 rounded-2xl border-sky-500 transition hover:bg-white">Get Started</button> </Link>
+          </div>
         </div>
+
       </div>
 
       <hr className="h-px my-8 bg-sky-400 border-0"/>
 
-      <main className="flex flex-col justify-center justify-items-center items-center gap-12">
+      <main className="flex flex-col justify-center justify-items-center items-center">
         <div className="flex justify-center justify-items-center items-center">
 
-          <div className="flex flex-col flex-wrap w-10/12 justify-around gap-8">
+          <div className="flex flex-col flex-wrap w-10/12 justify-around">
 
-                <div className="flex flex-col rounded rounded-xl border border-4 border-sky-500 text-xl">
-                  <div className="flex flex-row gap-8 justify-center justify-items-center items-center text-center">
-                    <Image className="w-1/3" src={hha2} alt="hha2"/>
+                <div className="flex flex-col text-xl">
+                  <div className="flex flex-row justify-center justify-items-center items-center text-center h-screen gap-40">
+                    <Image className="w-1/2 rounded rounded-full" src={hha2} alt="hha2"/>
                     <div className="text-center">
-                      Welcome to Vero Lady HHA Services, your trusted partner in personalized home health care services. At Vero Lady HHA Services, we understand that every individual&apos;s journey to wellness is unique. That&apos;s why we&apos;re dedicated to providing compassionate and tailored care that promotes independence, enhances quality of life, and brings peace of mind to both our clients and their families.
+                      At Vero Lady HHA Services, we understand that every individual&apos;s journey to wellness is unique. That&apos;s why we&apos;re dedicated to providing compassionate and tailored care that promotes independence, enhances quality of life, and brings peace of mind to both our clients and their families.
                     </div>
                   </div>
 
-                  <div className="flex flex-row gap-8 justify-center justify-items-center items-center text-center">
+                  <div className="flex flex-row gap-40 justify-center justify-items-center items-center text-center h-screen">
                     <div className="text-center">
                       Our team of highly-trained caregivers is committed to delivering top-notch medical and non-medical assistance within the comfort of your own home. Whether you or your loved one are recovering from an illness, managing a chronic condition, or simply in need of a helping hand, we&apos;re here to provide the support you deserve.
                     </div>
-                    <Image className="w-1/3" src={hha1} alt="hha1"/>
+                    <Image className="w-1/2 rounded rounded-full" src={hha1} alt="hha1"/>
                   </div>
 
-                  <div className="flex flex-row gap-8 justify-center justify-items-center items-center text-center">
-                    <Image className="w-1/3" src={hha4} alt="hha4"/>
+                  <div className="flex flex-row gap-40 justify-center justify-items-center items-center text-center h-screen">
+                    <Image className="w-1/2 rounded rounded-full" src={hha4} alt="hha4"/>
                     <div className="text-center">
                       With a deep respect for the dignity and preferences of each person we serve, Vero Lady HHA Services offers a wide range of services that encompass everything from skilled nursing care to companionship and light housekeeping. Our holistic approach not only addresses physical needs but also fosters emotional well-being, ensuring a comprehensive and holistic caregiving experience.
                     </div>
                   </div>
 
-                  <div className="flex flex-row gap-8 justify-center justify-items-center items-center text-center">
+                  <div className="flex flex-row gap-40 justify-center justify-items-center items-center text-center h-screen">
                     <div className="text-center">
                       As a locally-owned and operated small business, we take pride in our strong ties to the community we serve. Our mission is to create a warm and nurturing environment where our clients feel valued and understood. We believe that everyone deserves to age gracefully and comfortably in the place they call home, and we&apos;re honored to be a part of that journey.
                     </div>
-                    <Image className="w-1/3" src={hha3} alt="hha3"/>
+                    <Image className="w-1/2 rounded rounded-full" src={hha3} alt="hha3"/>
                   </div>
                 </div>
 
