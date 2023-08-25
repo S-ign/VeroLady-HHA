15dede916b82441fd2ac257db17e756fcb3c4044
Avery <avery.carty7@gmail.com>
2023-08-25 15:32:33 -0400

    update: Removed actions

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -1,26 +1,24 @@
-import type { GetServerSideProps } from 'next'
-import { Client } from "@hubspot/api-client";
-import { SimplePublicObject } from '@hubspot/api-client/lib/codegen/crm/companies';
-
-export type FormData = {
-	firstname: string;
-	lastname: string;
-	phone: string;
-	email: string;
-}
-
-export const createObject = async (data: FormData) => {
-	const hubspotClient = new Client({ accessToken:  process.env.HUBSPOT_TOKEN });
-	var properties = {
-		"firstname": data.firstname,
-		"lastname": data.lastname,
-		"phone": data.phone,
-		"email": data.email
-	}
-	const SimplePublicObjectInputForCreate = {
-		properties,
-		associations: [] 
-	};
-
-	const apiResponse = await hubspotClient.crm.contacts.basicApi.create(SimplePublicObjectInputForCreate); console.log(JSON.stringify(apiResponse, null, 2));
-}
+//import { Client } from "@hubspot/api-client";
+//
+//export type FormData = {
+//	firstname: string;
+//	lastname: string;
+//	phone: string;
+//	email: string;
+//}
+//
+//export const createObject = async (data: FormData) => {
+//	const hubspotClient = new Client({ accessToken:  process.env.HUBSPOT_TOKEN });
+//	let properties = {
+//		"firstname": data.firstname,
+//		"lastname": data.lastname,
+//		"phone": data.phone,
+//		"email": data.email
+//	}
+//	const SimplePublicObjectInputForCreate = {
+//		properties,
+//		associations: [] 
+//	};
+//
+//	const apiResponse = await hubspotClient.crm.contacts.basicApi.create(SimplePublicObjectInputForCreate); console.log(JSON.stringify(apiResponse, null, 2));
+//}
